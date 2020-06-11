4a4201411dc7216f0e49831d15d4a564212a9872
Victor Manuel Briones <victorm140399@gmail.com>
2020-06-10 22:13:55 -0500

    Consumiendo una REST API con Fetch API

--- FETCHAPI/js/app.js
+++ FETCHAPI/js/app.js
@@ -1,4 +1,6 @@
 document.getElementById('txtBtn').addEventListener('click', cargarTXT);
+document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
+document.getElementById('apiBTN').addEventListener('click', cargarREST);
 
 function cargarTXT(){
     fetch('datos.txt')
@@ -12,4 +14,43 @@ function cargarTXT(){
         .catch(function(error){
             console.log(error);
         });
+}
+
+function cargarJSON(){
+    fetch('empleados.json')
+       .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            let html = '';
+            data.forEach(function(empleado) {
+                html += `
+                    <li>${empleado.nombre} ${empleado.puesto}</li>
+                `;
+           })
+            document.getElementById('resultado').innerHTML = html;
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+}
+
+function cargarREST(){
+    fetch('https://picsum.photos/list')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(imagenes){
+            let html = '';
+
+            imagenes.forEach(function(imagen){
+                html += `
+                    <li>
+                        <a target="_blank" href="${imagen.post_url}">Ver Imagen</a>
+                        ${imagen.author}
+                    </li>
+                `;
+            });
+            document.getElementById('resultado').innerHTML = html;
+        })
 }
